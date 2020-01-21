9a7a62236d10308a7facd87f1721c90e2360853d
Sathish Bopini <bopini@gmail.com>
2020-01-21 10:00:31 -0800

    First Enhancement

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MacInfo Application
 
-This repo contains a script that gets Company Name information by providing MAC address as argument <br>
+MacInfo Application outputs Company Name information by providing MAC address as input
 
 Note: Only Traditional 12-digit MAC addresses are considered in this script
 
@@ -15,8 +15,8 @@ Note: Only Traditional 12-digit MAC addresses are considered in this script
 1. Run the container <br>
 ```docker run -it macinfo:1 sh```
 2. Run the script<br>
-    ```python3 find_mac_info.py BC-92-6B-A0-00-01``` <br>
-   Mac address arguments supported in the format of<br>```BC-92-6B-A0-00-01 or BC:92:6B:A0:00:01```
+    ```python3 find_mac_info.py --mac_address=BC-92-6B-A0-00-01``` <br>
+   Mac address arguments supported in the format of<br>```BC-92-6B-A0-00-01 or BC:92:6B:A0:00:01 or BC926BA00001```
 
 ## Run Doc Test - Limited coverage
 
@@ -25,13 +25,5 @@ Note: Only Traditional 12-digit MAC addresses are considered in this script
 
 ## Enhancements:
 
-### High Priority:
 1. Security : Need to use  hardened docker image. For time being selected alpine image since it is light weight
 2. Support 64 bit MAC address
-3. DocTest is implemented but need additonal test cases <br>
-   ```python -m doctest -v find_mac_info.py```
-   
-### Low:
-1. Handle more exceptions
-2. Shall be written in object oriented
-3. Improve arg parser for usage, for now using simple print statement
--- find_mac_info.py
+++ find_mac_info.py
@@ -1,65 +1,146 @@
+import argparse
 import urllib.request as urllib2
 import json
 import codecs
 import re
 import sys
-import argparse
 
-def getMacAddressInput():
-    """ Get Mac Address from User Input"""
-    #parser = argparse.ArgumentParser(description='Mac Address application')
-    if len(sys.argv) < 2:
-        print('Please provide Mac address as argument')
-        sys.exit()
-    elif len(sys.argv) > 2:
-        print('You have specified too many arguments')
-        sys.exit()
-    else:
-        mac_address = sys.argv[1]
-    return mac_address
+class MacAddressInfo(object):
 
-def getCompanyName(mac_address):
     """
-    >>> getCompanyName('BC:92:6B:A0:00:01')
-    'Apple, Inc.'
-    >>> getCompanyName('BC-92-6B-A0-00')
-    None
+       This is a class which provides company name by taking mac address as input .
+
+       Attributes:
+            None
     """
-    valid = validateMacAddress(mac_address)
-    if valid:
-        url = "http://macvendors.co/api/"
-        try:
-            request = urllib2.Request(url+mac_address, headers={'User-Agent' : "API Browser"})
-            response = urllib2.urlopen( request )
-            reader = codecs.getreader("utf-8")
-            obj = json.load(reader(response))
-            return (obj['result']['company'])
-        except urllib2.HTTPError as e:
-            print(e)
-    else:
-        print('Not a valid MAC Address.')
-        print('Only Traditional 12-digit MAC addresses is supported.')
-        print('Example: BC-92-6B-A0-00-01 or BC:92:6B:A0:00:01')
-        sys.exit()
 
-def validateMacAddress(mac_address):
-    """ Validate Mac Address"""
-    if re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", mac_address.lower()):
-        return True
-    else:
-        return False
+    def __init__(self, mac_address, company_name=None):
+        """
+        The constructor for MacAddressInfo class.
 
-def output(mac_address, company_name):
-    """ Output company name for the mac address"""
-    print("Mac Address: " + mac_address)
-    print("Company Name: " + company_name)
+        Parameters:
+           mac_address : Mac address
+           company_name : None
+        """
+
+        self.mac_address = mac_address
+        self.company_name = company_name
+
+    def getCompanyName(self):
+        """
+        The function to get Company Name.
+
+        Parameters:
+            none.
+
+        Returns:
+            Company name
+
+        >>> test1 = MacAddressInfo('BC:92:6B:A0:00:01')
+        >>> test1.getCompanyName()
+        'Apple, Inc.'
+        >>> test2 = MacAddressInfo('BC-92-6B-A0-00-01')
+        >>> test2.getCompanyName()
+        'Apple, Inc.'
+        >>> test3 = MacAddressInfo('BC926BA00001')
+        >>> test3.getCompanyName()
+        'Apple, Inc.'
+        >>> test4 = MacAddressInfo('BC-92-6B-A0-00')
+        >>> test4.getCompanyName()
+        Not a valid MAC Address
+        Only Traditional 12-digit MAC addresses is supported
+        Example: BC-92-6B-A0-00-01 or BC:92:6B:A0:00:01 or BC926BA00001
+        """
+
+        valid = self.validateMacAddress()
+        if valid:
+            url = "http://macvendors.co/api/"
+            try:
+                request = urllib2.Request(url+self.mac_address, headers={'User-Agent' : "API Browser"})
+                response = urllib2.urlopen( request )
+                reader = codecs.getreader("utf-8")
+                obj = json.load(reader(response))
+                if  "company" in obj['result']:
+                    self.company_name = obj['result']['company']
+                return self.company_name
+            except urllib2.HTTPError as e:
+                print('The server couldn\'t fulfill the request.')
+                print('Error code: ', e.code)
+            except urllib2.URLError as e:
+                print('We failed to reach a server.')
+                print('Reason: ', e.reason)
+        else:
+            str="Not a valid MAC Address\nOnly Traditional 12-digit MAC addresses is supported\nExample: BC-92-6B-A0-00-01 or BC:92:6B:A0:00:01 or BC926BA00001"
+            print(str)
+            return None
+            sys.exit()
+
+    def validateMacAddress(self):
+        """
+        The function to validate Mac address.
+
+        Parameters:
+            none.
+
+        Returns:
+            Boolean value by validating address
+
+        >>> test1 = MacAddressInfo('BC:92:6B:A0:00:01')
+        >>> test1.validateMacAddress()
+        True
+        >>> test2 = MacAddressInfo('BC-92-6B-A0-00-01')
+        >>> test2.validateMacAddress()
+        True
+        >>> test3 = MacAddressInfo('BC-92-6B-A0-00-01')
+        >>> test3.validateMacAddress()
+        True
+        >>> test4 = MacAddressInfo('BC-92-6B-A0-00')
+        >>> test4.validateMacAddress()
+        False
+        """
+
+        if re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", self.mac_address.lower()):
+            return True
+        else:
+            return False
+
+    def displayOutput(self, mac_address, company_name):
+        """
+        The function to Display Output
+
+        Parameters:
+            mac_address
+            company_name
+
+        Returns:
+            Output of mac_address and company_name
+
+        >>> address = 'BC:92:6B:A0:00:01'
+        >>> test1 = MacAddressInfo(address)
+        >>> out = test1.getCompanyName()
+        >>> test1.displayOutput(address, out)
+        Mac Address: BC:92:6B:A0:00:01
+        Company Name: Apple, Inc.
+        """
+
+        print("Mac Address: " + self.mac_address)
+        print("Company Name: " + self.company_name)
 
 def main():
-    """ Main """
-    mac_address = getMacAddressInput()
-    company_name = getCompanyName(mac_address)
-    if company_name:
-        output(mac_address, company_name)
 
-if __name__ == "__main__":
+    """ Main Function. Validate arguments through argparser """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mac_address', required=True)
+    args = parser.parse_args()
+    address=args.mac_address
+
+    obj = MacAddressInfo(address)
+    company_name = obj.getCompanyName()
+    if company_name:
+        obj.displayOutput(address, company_name)
+    else:
+        print("Company name is not found for given Mac address")
+
+if __name__ == '__main__':
+    """ """
     main()
